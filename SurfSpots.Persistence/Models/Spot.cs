8b584e479c21727a3af18002347f4a67986509c9
Marios <Mariosi@hotmail.co.uk>
2020-05-13 10:38:07 +0100

    WebApi and Persistence Layer Initial Commit

---
+++ SurfSpots.Persistence/Models/Spot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurfSpots.Persistence.Models
+{
+	public class Spot
+	{
+		public int Id { get; set; }
+		public string Name {get; set; }
+		public string Country { get; set; }
+		public string Region { get; set; }
+		public string Tides { get; set; }
+		public string BestTide { get; set; }
+		public double MinSwell { get; set; }
+		public double MaxSwell { get; set; }
+		public string SwellDirection { get; set; }
+		public string BestSwellDirection { get; set; }
+		public string Notes { get; set; }
+	}
+}
---
+++ SurfSpots.Persistence/SurfSpotsDBContext.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SurfSpots.Persistence.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SurfSpots.Persistence
+{
+	public class SurfSpotsDBContext : DbContext
+	{
+		public DbSet<Spot> Spots { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"Data Source=(localdb)\v13.0;Initial Catalog=SpotsDb;");
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Spot>(entity =>
+			{
+				entity.ToTable("Spots");
+
+				entity.Property(e => e.Name)
+					.IsRequired()
+					.HasColumnName("Name")
+					.HasColumnType("varchar(100)");
+
+
+
+			});
+
+			modelBuilder.Entity<Spot>().HasData(
+				new Spot
+				{
+					Id = 1,
+					Name = "Ribeira D'Ilhas",
+					Country = "Portugal",
+					Region = "Lisbon",
+					Tides = "All",
+					BestTide = "Mid",
+					MinSwell = 0.5,
+					MaxSwell = 3,
+					SwellDirection = "180-360",
+					BestSwellDirection = "",
+					Notes = ""
+				},
+				new Spot
+				{
+					Id = 2,
+					Name = "Sao Juliao - The Left",
+					Country = "Portugal",
+					Region = "Lisbon",
+					Tides = "Low",
+					BestTide = "Low",
+					MinSwell = 1,
+					MaxSwell = 3,
+					SwellDirection = "180-360",
+					BestSwellDirection = "",
+					Notes = ""
+				},
+				new Spot
+				{
+					Id = 3,
+					Name = "Mawi",
+					Country = "Indonesia",
+					Region = "Lombok",
+					Tides = "All",
+					BestTide = "Mid - High",
+					MinSwell = 0.5,
+					MaxSwell = 5,
+					SwellDirection = "220-270",
+					BestSwellDirection = "",
+					Notes = ""
+				}
+			);
+		}
+	}
+}
